ebbd1184b38de1528deb4c200aefb26f3b5122ae
alextzh <tianzhihong110@126.com>
2017-12-04 16:14:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res) {
+	res.sendfile('index.html')
+})
+
+io.on('connection', function(socket) {
+	console.log('a user connected')
+	socket.on('notification', function(msg) {
+		console.log(msg)
+		io.emit('notification', msg)
+	})
+	socket.on('disconnect', function () {
+		console.log('user disconnected')
+  })
+})
+
+http.listen(8080, function() {
+	console.log('listening on port 8080')
+})
